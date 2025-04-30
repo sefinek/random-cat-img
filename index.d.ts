103a5cfdf51824e9b1848da8a087cf2384f0b201
Sefinek <contact@sefinek.net>
2025-04-30 12:30:50 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,18 @@
-declare module 'random-cat-img' {
-    interface Info {
-        category: string;
-        endpoint: string;
-    }
-
-    interface GetRandomCat {
-        success: boolean;
-        status: number;
-        info: Info;
-        message: string;
-    }
-
-    /**
-     * Retrieves a random cat object from the specified API.
-     *
-     * @async
-     * @returns {Promise<GetRandomCat>} A promise that resolves with a random cat object on success or rejects with an error on failure.
-     * @throws {Error} If there's an error in making the request, parsing JSON data, or if the API responds with a non-200 status code.
-     */
-    export function getRandomCat(): Promise<GetRandomCat>;
+export interface Info {
+    category: string;
+    endpoint: string;
 }
+
+export interface GetRandomCat {
+    success: boolean;
+    status: number;
+    info: Info;
+    message: string;
+}
+
+/**
+ * Fetches a random cat image from the API.
+ * @returns A promise that resolves to a random fox object on success.
+ * @throws If the request fails, the response is invalid, or JSON parsing fails.
+ */
+export function getRandomCat(): Promise<GetRandomCat>;
